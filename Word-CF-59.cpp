ef505ad7bd4713eda6c2bc87b242160802a991a8
Kushagra Saxena <144206868+kushagra-1010@users.noreply.github.com>
2026-01-05 19:37:35 +0530

    Add files via upload

---
+++ Word-CF-59.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    
+    int countUpper = 0;
+    int countLower = 0;
+    
+    for(char c : s){
+        if(islower(c)){
+            countLower++;
+        }else{
+            countUpper++;
+        }
+    }
+    
+    if(countUpper > countLower){
+        for(char& c : s){
+            c = toupper(c);
+        }
+    }else{
+        for(char& c : s){
+            c = tolower(c);
+        }
+    }
+    
+    cout << s << endl;
+    
+    return 0;
+}

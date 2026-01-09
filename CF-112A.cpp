1de64a3a2f23499708dde6aa3ccbedb177eb3c5b
Kushagra Saxena <144206868+kushagra-1010@users.noreply.github.com>
2026-01-09 23:58:00 +0530

    Add files via upload

---
+++ CF-112A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    
+    // Compare character by character
+    for (int i = 0; i < s1.length(); i++) {
+        char c1 = s1[i];
+        char c2 = s2[i];
+        
+        // Convert to lowercase if uppercase
+        if (c1 >= 'A' && c1 <= 'Z') {
+            c1 = c1 + ('a' - 'A');  // or c1 = c1 + 32
+        }
+        if (c2 >= 'A' && c2 <= 'Z') {
+            c2 = c2 + ('a' - 'A');  // or c2 = c2 + 32
+        }
+        
+        if (c1 < c2) {
+            cout << -1 << endl;
+            return 0;
+        } else if (c1 > c2) {
+            cout << 1 << endl;
+            return 0;
+        }
+    }
+    
+    cout << 0 << endl;
+    return 0;
+}
